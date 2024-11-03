5ed1e19cdf15320ee8f0be3a50792cbf540a8042
rehan606 <rehanlemu@gmail.com>
2024-11-04 03:33:59 +0600

    Filter by Category

--- src/components/GadgetCards.jsx
+++ src/components/GadgetCards.jsx
@@ -1,16 +1,23 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Card from "./Card";
+import { useEffect, useState } from "react";
 
 
 const GadgetCards = () => {
     const data = useLoaderData()
     const {category} = useParams()
+    const [gadget, setGadget] = useState([])
+
+    useEffect(()=> {
+        const filterByCategory = [...data].filter(gadget => gadget.category === category)
+        setGadget(filterByCategory)
+    }, [category, data])
     
     return (
         <div className="container w-12/12 mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {
-                    data.map(gadget => <Card key={gadget.id} gadget={gadget}></Card> )
+                    gadget.map(gadget => <Card key={gadget.id} gadget={gadget}></Card> )
                 }
             </div>
         </div>
