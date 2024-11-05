5c268d963d5d1dce6522aefb540c03fa9a8b8bc0
rehan606 <rehanlemu@gmail.com>
2024-11-06 01:17:27 +0600

    Category item not found page

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -10,14 +10,14 @@ const Categories = ({categories}) => {
              tabs tabs-boxed border border-gray-300 bg-white p-5 rounded-xl space-y-3 flex flex-col  w-[250px]">
                 
                 {
-
-
                     categories.map(category=> (
                     <NavLink
                          key={category.category} to={`/category/${category.category}`} role="tab" 
                          className={({isActive})=> `tab w-full h-12 !rounded-full font-semibold bg-gray-200 hover:bg-[#4b39f5] hover:text-white ${isActive ? 'tab-active' : ''}`}>{category.category}
                     </NavLink> ))
                 }
+
+                
             </div>
         </div>
     );
--- src/components/GadgetCards.jsx
+++ src/components/GadgetCards.jsx
@@ -18,12 +18,25 @@ const GadgetCards = () => {
     }, [category, data])
     
     return (
-        <div className="container w-12/12  pr-7">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {
-                    gadget.map(gadget => <Card key={gadget.id} gadget={gadget}></Card> )
-                }
-            </div>
+        // <div className="container w-12/12  pr-7">
+        //     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        //         {
+        //             gadget.map(gadget => <Card key={gadget.id} gadget={gadget}></Card> )
+        //         }
+        //     </div>
+        // </div>
+
+        <div className="container w-12/12 pr-7">
+            {gadget.length > 0 ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {gadget.map(gadget => <Card key={gadget.id} gadget={gadget}></Card>)}
+                </div>
+            ) : (
+                <div className=" flex flex-col justify-center items-center">
+                    <p className="text-center text-3xl font-bold mb-3 text-primary">Item Not Found</p>
+                    <img src="https://www.realestatedirectory.in/image/no-data.gif" alt="" className="w-96 h-[400px] rounded-xl border border-green-400"/>
+                </div>
+            )}
         </div>
     );
 };
